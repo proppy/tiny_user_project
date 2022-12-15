cb931e76cb57deab3c0ae3464e703627bfc8354c
Johan Euphrosine <proppy@google.com>
2022-12-15 17:50:45 +0900

    remove wb and la pins, pass io_in[0] as clk

--- verilog/rtl/tiny_user_project.v
+++ verilog/rtl/tiny_user_project.v
@@ -9,40 +9,16 @@ module tiny_user_project(
     inout vccd1,
     inout vssd1,
 `endif
-
-    // Wishbone Slave ports (WB MI A)
-    input wb_clk_i,
-    input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
-
-    // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
-
     // IOs
     input  [`MPRJ_IO_PADS-1:0] io_in,
     output [`MPRJ_IO_PADS-1:0] io_out,
     output [`MPRJ_IO_PADS-1:0] io_oeb,
-
-    // Independent clock (on independent integer divider)
-    input   user_clock2,
-
-    // User maskable interrupt signals
-    output [2:0] user_irq
 );
 
 // pass input and output pins defined in user_defines.v
 user_module_334445762078310996 mod (
-    io_in[15:8],
-    io_out[23:16]
+    .io_in (io_in[15:8]),
+    .io_out(io_out[23:16])
 );
 // all output enabled
 assign io_oeb[23:16] = 8'b0;
--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -87,37 +87,10 @@ tiny_user_project mprj (
 	.vccd1(vccd1),	// User area 1 1.8V power
 	.vssd1(vssd1),	// User area 1 digital ground
 `endif
-
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
-
-    // MGMT SoC Wishbone Slave
-
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
-
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
     // IO Pads
-
     .io_in (io_in),
     .io_out(io_out),
     .io_oeb(io_oeb),
-
-    .user_clock2(user_clock2),
-			
-    // IRQ
-    .user_irq(user_irq)
 );
 
 endmodule	// user_project_wrapper
